71f5785b309e10ae507ce47bd104f0be21748588
thang nguyenminh <tt.thang.nguyen@tomosia.com>
2021-08-30 09:53:48 +0700

    add middreware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,9 +1,9 @@
 export default function ({ $auth, route, redirect }) {
-  // if (!$auth.$storage.getUniversal('token') && route.name !== 'login') {
-  //   redirect('/login')
-  // } else if (
-  //   !$auth.$storage.getUniversal('token').roles.includes('ROLE_ADMIN')
-  // ) {
-  //   redirect('/')
-  // }
+  if (!$auth.$storage.getUniversal('token') && route.name !== 'login') {
+    redirect('/login')
+  } else if (
+    !$auth.$storage.getUniversal('token').roles.includes('ROLE_ADMIN')
+  ) {
+    redirect('/')
+  }
 }
